7af37b82ec5e97750913b8851574634016ed9dd3
nzayatz14 <csc101@warcraft-2.local>
2015-10-16 14:43:15 -0400

    made time limit dynamic

--- Source/MazeGameEnhanced/MyHUD.cpp
+++ Source/MazeGameEnhanced/MyHUD.cpp
@@ -26,6 +26,14 @@ AMyHUD::AMyHUD(const class FObjectInitializer &PCIP):Super(PCIP)
 void AMyHUD::BeginPlay(){
     Super::BeginPlay();
     
+    //move player start to that location
+    TActorIterator<AMaze> ActorItr =TActorIterator<AMaze>(GetWorld());
+    
+    //check to see of the user has won
+    if (ActorItr) {
+        time = ActorItr->numRows * 10;
+    }
+    
     //set up timer
     GetWorld()->GetTimerManager().SetTimer(handleClock, this, &AMyHUD::showTime, 1.0f, true);
 }
